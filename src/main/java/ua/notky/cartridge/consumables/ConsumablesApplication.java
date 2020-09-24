250c625bfe0fd5041eef39339a5aed3f6f5492be
notky <notky@ukr.net>
2020-09-24 17:17:52 +0300

    added configuration for logger

--- src/main/java/ua/notky/cartridge/consumables/ConsumablesApplication.java
+++ src/main/java/ua/notky/cartridge/consumables/ConsumablesApplication.java
@@ -1,13 +1,19 @@
 package ua.notky.cartridge.consumables;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ConsumablesApplication {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ConsumablesApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(ConsumablesApplication.class, args);
+
+        LOGGER.info("Application is running...");
+
     }
 
 }
