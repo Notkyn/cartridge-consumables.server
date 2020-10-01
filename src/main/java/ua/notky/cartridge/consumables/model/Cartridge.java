db21c806cebdd57a0160bc557aeca92db5c14126
notky <notky@ukr.net>
2020-10-01 17:01:42 +0300

    added:  * model - Cartridge          - Department

---
+++ src/main/java/ua/notky/cartridge/consumables/model/Cartridge.java
@@ -0,0 +1,6 @@
+package ua.notky.cartridge.consumables.model;
+
+public class Cartridge extends AbstractNameEntity {
+    private static final long serialVersionUID = 4466775734839249457L;
+
+}
---
+++ src/main/java/ua/notky/cartridge/consumables/model/Department.java
@@ -0,0 +1,5 @@
+package ua.notky.cartridge.consumables.model;
+
+public class Department extends AbstractNameEntity {
+    private static final long serialVersionUID = -8823196802859515157L;
+}
