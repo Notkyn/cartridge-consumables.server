19a425b22f1741cf05305734d9394097f187dd0c
notky <notky@ukr.net>
2020-10-16 17:35:54 +0300

    updated:  * Department model: added all constructors added:  * test tool: department model tool with test data

--- src/main/java/ua/notky/cartridge/consumables/model/Department.java
+++ src/main/java/ua/notky/cartridge/consumables/model/Department.java
@@ -7,4 +7,15 @@ import javax.persistence.Table;
 @Table(name = "department")
 public class Department extends AbstractNameEntity {
     private static final long serialVersionUID = -8823196802859515157L;
+
+    public Department() {
+    }
+
+    public Department(String name) {
+        super(name);
+    }
+
+    public Department(Integer id, String name) {
+        super(id, name);
+    }
 }
---
+++ src/test/java/ua/notky/cartridge/consumables/tools/data/DepartmentTool.java
@@ -0,0 +1,31 @@
+package ua.notky.cartridge.consumables.tools.data;
+
+import ua.notky.cartridge.consumables.model.Department;
+
+import java.util.List;
+
+public class DepartmentTool {
+    public static final int ID_DEPARTMENT_2 = 2;
+
+    public static final Department DEPARTMENT_1 = new Department(1, "department_1");
+    public static final Department DEPARTMENT_2 = new Department(ID_DEPARTMENT_2, "department_1");
+    public static final Department DEPARTMENT_3 = new Department(3, "department_1");
+    public static final Department DEPARTMENT_4 = new Department(4, "department_1");
+    public static final Department DEPARTMENT_5 = new Department(5, "department_1");
+
+    public static List<Department> DEPARTMENTS = List.of(DEPARTMENT_1, DEPARTMENT_2, DEPARTMENT_3, DEPARTMENT_4, DEPARTMENT_5);
+
+    public static Department getNew(){
+        return new Department("New Department");
+    }
+
+    private static Department getCopy(Department value){
+        return new Department(value.getId(), value.getName());
+    }
+
+    public static Department getUpdated(Department value){
+        Department updated = getCopy(value);
+        updated.setName("Updated Department");
+        return updated;
+    }
+}
